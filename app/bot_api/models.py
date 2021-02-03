d0c16f5a58f878ceefbeda6ace3a4c1e24d345bb
boleque <simple108@mail.ru>
2021-02-03 22:09:19 +0300

    Added unit test for Chat model

--- app/bot_api/models.py
+++ app/bot_api/models.py
@@ -1,6 +1,5 @@
 from django.db import models, migrations
 from django.contrib.postgres.fields import HStoreField
-from model_utils import Choices
 
 
 class CommonResponse(models.Model):
@@ -16,8 +15,29 @@ class Greeting(CommonResponse):
 class Fallback(CommonResponse):
     pass
 
+class DialogueTypes(models.TextChoices):
+    SAMPLES = 'SAMPLES', 'samples'
+    KEYWORDS = 'KEYWORDS', 'keywords'
+
 class Dialogue(CommonResponse):
     parent_id = models.CharField(max_length=100, default=str)
-    dialogue_type = models.CharField(max_length=20, choices=Choices('SAMPLES', 'KEYWORDS'))
+    dialogue_type = models.CharField(max_length=20, choices=DialogueTypes.choices)
     keywords = HStoreField(default=dict)
     samples = HStoreField(default=dict)
+
+class Language(models.TextChoices):
+    ENGLISH = 'en', 'English'
+    NORWEGIAN = 'nb', 'Norwegian'
+
+class Chat(models.Model):
+    user_id = models.IntegerField(primary_key=True)
+    language = models.CharField(max_length=20, choices=Language.choices)
+    last_reply_id = models.CharField(max_length=100)
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                name="%(app_label)s_%(class)s_language_valid",
+                check=models.Q(language__in=Language.values),
+            )
+        ]
--- app/bot_api/tests/test_models.py
+++ app/bot_api/tests/test_models.py
@@ -1,7 +1,31 @@
 from django.test import TestCase
+from django.db.utils import IntegrityError
+from .. models import Chat
 
 class ModelsTests(TestCase):
 
-    def create_configuration_model_successful():
-        """ Test creating congfiguration model successful """
-        pass
+    def test_create_chat_model_success(self):
+
+        input_data = {'user_id': 1, 'language': 'en'}
+        chat = Chat.objects.create(
+            user_id=input_data['user_id'],
+            language=input_data['language']
+        )
+        self.assertEqual(chat.user_id, input_data['user_id'])
+        self.assertEqual(chat.language, input_data['language'])
+    
+    def test_create_chat_model_wrong_lang(self):
+
+        input_data = {'user_id': 1, 'language': 'FI'}
+        chat = Chat(
+            user_id=input_data['user_id'],
+            language=input_data['language']
+        )
+
+        try:
+            chat.save()
+        except Exception as err:
+            self.assertEqual(IntegrityError, type(err))
+
+        #self.assertEqual(chat.user_id, input_data['user_id'])
+        #self.assertEqual(chat.language, input_data['language'])
